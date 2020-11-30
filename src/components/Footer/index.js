a4d6c79b23a39279b3f13760c01262d5c2c415fa
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-11-30 18:53:42 -0500

    Created footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+// Dependencies
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="footer">
+        <span>Employee Directory</span>
+      </footer>
+    );
+};
+
+export default Footer;
