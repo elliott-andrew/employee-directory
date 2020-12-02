3deb975fa326db26016ee45b93f2bb0b4c47ffc3
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-12-02 16:47:51 -0500

    Updated Employees page rendering

--- src/pages/Employees/index.js
+++ src/pages/Employees/index.js
@@ -1,43 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Employees extends Component {
-  handleClick() {
-    console.log("Clicked")
-  }
-    render() {
-        return (
-            <table id="myTable" class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th onClick={this.handleClick} scope="col">Name</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-        )
-    };
+function Employees() {
+  return(
+    <table id="myTable" class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+  )
 };
 
 export default Employees;
