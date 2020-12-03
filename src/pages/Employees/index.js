9914ad472f5456ea11ea986b0b120ea46c3fe5c4
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-12-02 20:41:14 -0500

    Updated map to pull from the sortedEmployees array.

--- src/pages/Employees/index.js
+++ src/pages/Employees/index.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 
-
-
 function Employees() {
   // First we create an array of employees to use in our table
   const employees =
@@ -9,45 +7,48 @@ function Employees() {
       {
         id: 1,
         firstName: "Hem",
-        lastName: "The Cat",
+        lastName: "Cat",
         role: "Bodega Cat"
       },
       {
         id: 2,
         firstName: "Kyle",
-        lastName: "The Cat",
+        lastName: "Cat",
         role: "Bodega Cat"
       },
       {
         id: 3,
         firstName: "Colonel",
-        lastName: "The Dog",
+        lastName: "Dog",
         role: "Loud Borker"
       },
       {
         id: 4,
         firstName: "Cheif",
-        lastName: "The Dog",
+        lastName: "Dog",
         role: "Food Scavenger"
       }
     ];
   // Create an array to hold the employees array, which we can then use to adjust the order
   let sortedEmployees = [...employees];
-  // We call on the array and pass two employees into the sorting function. This compares two employees and sorts them based the firstName prop.
-  sortedEmployees.sort((eOne, eTwo) => {
-    if (eOne.firstName < eTwo.firstName) {
-      return -1;
-    } else if (eOne.firstName > eTwo.firstName) {
-      return 1;
-    } else return 0
-  });
-
+  // Establish useState
   const [sortedRow, setSortedRow] = useState();
+  // We call on the array and pass two employees into the sorting function. This compares two employees and sorts them based the firstName prop.
+  if (sortedRow) {
+    sortedEmployees.sort((eOne, eTwo) => {
+      if (eOne[sortedRow] < eTwo[sortedRow]) {
+        return -1;
+      } else if (eOne[sortedRow] > eTwo[sortedRow]) {
+        return 1;
+      } else return 0
+    });
+  };
 
   return (
-    <table id="myTable" class="table">
+    <table id="myTable" className="table">
       <thead>
         <tr>
+          {/* onClick, pass the name of the row selected to sortedEmployees.sort() */}
           <th scope="col"><button onClick={() => setSortedRow('id')}>#</button></th>
           <th scope="col"><button onClick={() => setSortedRow('firstName')}>First Name</button></th>
           <th scope="col" onClick={() => setSortedRow('lastName')}><button>Last Name</button></th>
@@ -55,15 +56,14 @@ function Employees() {
         </tr>
       </thead>
       <tbody>
-
-        {employees.map((employee => (
+        {sortedEmployees.map((employee => (
           // Map through our employees array and create a tr/td for each employees in the array.
           // Establish unique key, display id number, first name, last name and role.
           <tr key={employee.id}>
-            <td>{employee.id}</td>
-            <td>{employee.firstName}</td>
-            <td>{employee.lastName}</td>
-            <td>{employee.role}</td>
+            <td key={employee.id}>{employee.id}</td>
+            <td key={employee.firstName}>{employee.firstName}</td>
+            <td key={employee.lastName}>{employee.lastName}</td>
+            <td key={employee.role}>{employee.role}</td>
           </tr>
         )))}
       </tbody>
